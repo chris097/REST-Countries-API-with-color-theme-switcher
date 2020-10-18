abc8d57ea07d0b512b6050d896c1baf5fe6e55df
chris097 <chrisfidel.international@gmail.com>
2020-10-18 19:31:16 +0100

    update country

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios";
 import "./Country.css";
+import mapp from "./img/hacker3.jpg";
 
 const Country = () => {
+    const [ country, setCountry ] = useState([])
+
+    useEffect(() => {
+        axios.get("https://restcountries.eu/rest/v2/all")
+        .then(res => console.log(res.data.name))
+
+    })
     return(
         <div className="container">
             <div className="search">
@@ -13,6 +22,49 @@ const Country = () => {
                         <option value="">Filter by Region</option>
                     </select>
                 </div>
+            </div>
+            <div className="cards-container">
+                <div className="cards-content">
+                    <div className="cards">
+                        <img src={mapp} alt=""/>
+                        <div className="country-id">
+                        <h4>Nigeria</h4>
+                        <h4>Population: <span>490,0494</span></h4>
+                        <h4>Region: <span>Africa</span></h4>
+                        <h4>Capital: <span>Abuja</span></h4>
+                        </div>
+                    </div>
+                    <div className="cards">
+                        <img src={mapp} alt=""/>
+                        <div className="country-id">
+                        <h4>Nigeria</h4>
+                        <h4>Population: <span>490,0494</span></h4>
+                        <h4>Region: <span>Africa</span></h4>
+                        <h4>Capital: <span>Abuja</span></h4>
+                        </div>
+                    </div>
+                    <div className="cards">
+                        <img src={mapp} alt=""/>
+                        <div className="country-id">
+                        <h4>Nigeria</h4>
+                        <h4>Population: <span>490,0494</span></h4>
+                        <h4>Region: <span>Africa</span></h4>
+                        <h4>Capital: <span>Abuja</span></h4>
+                        </div>
+                    </div>
+                    <div className="cards">
+                        <img src={mapp} alt=""/>
+                        <div className="country-id">
+                        <h4>Nigeria</h4>
+                        <h4>Population: <span>490,0494</span></h4>
+                        <h4>Region: <span>Africa</span></h4>
+                        <h4>Capital: <span>Abuja</span></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="country-btn">
+                <button type="submit">Read More</button>
             </div>
         </div>
     )
